8b34657d7b738a1d076882ebd0d77e0968d3e6c9
patelarth0311 <patelarth0311@gmail.com>
2024-01-18 13:52:10 -0500

    speed insights

--- src/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ src/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -32,12 +32,18 @@ import {
 import { AskAI } from "@/app/(main)/_components/askai";
 import { UserContext } from "@/app/context";
 import { DocumentView } from "@/app/(main)/_components/documentview";
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+
+type Repo = {
+  apiKey: string
+  
+}
 
 export const getServerSideProps = (async () => {
-  const apiKey = process.env.SECRETKEY
-  console.log(apiKey)
+  const repo: Repo  = {apiKey : process.env.SECRETKEY!}
+  return { props: { repo } }
  
-}) 
+}) satisfies GetServerSideProps<{ repo: Repo }>
 
 
 function DocumentPage({ params }: { params: { documentId: string } }) {
