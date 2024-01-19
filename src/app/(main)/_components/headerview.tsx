2d9484f85e8a314f384ed1009031eaf8c4ee7d68
patelarth0311 <patelarth0311@gmail.com>
2024-01-19 08:16:25 -0500

    speed insights

--- src/app/(main)/_components/headerview.tsx
+++ src/app/(main)/_components/headerview.tsx
@@ -67,16 +67,28 @@ export const HeaderView = ({
 
   const updateDocName = async (name: string, documentId: string) => {
 
-
     if (context) {
     
       fetch(`/api/cred?userId=${context.user.userId}&name=${name}&documentId=${documentId}`,{
         method: "GET",
     }).then((res) => res.json()).then(res => {
-      console.log(res)
+      dispatch(
+        setADocument({
+          updatedDocument: {
+            ...(res as Document),
+            editMessage: `Changed title to ${name}`,
+          },
+        })
+      );
+      makeToast({
+        ...(res as Document),
+        editMessage: `Changed title to ${name}`,
+      });
     })
 
     }
+   
+    
   };
 
   return (
